a97404a7ccdf9ac3baaa562e7c1199461ce5a7de
Hamnab02 <hamnab02@gmail.com>
2024-09-09 22:21:14 +0100

    updated app.js links

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function App() {
         <footer>
           This project was coded by{" "}
           <a
-            href=""
+            href="/"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -27,7 +27,7 @@ export default function App() {
           </a>{" "}
           and{" "}
           <a
-            href=""
+            href="/"
             target="_blank"
             rel="noopener noreferrer"
           >
