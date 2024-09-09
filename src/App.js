4335e169328845c856a0588e13cccab048a4bad2
Hamnab02 <hamnab02@gmail.com>
2024-09-09 19:45:29 +0100

    added a footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -16,6 +16,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          <footer>This project was coded by Hamna Babar</footer>
         </a>
       </header>
     </div>
